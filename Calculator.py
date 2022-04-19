fa6f28dd4f5aa700d51d7b6593cfef2378644aac
Maksimaka7 <103311378+Maksimaka7@users.noreply.github.com>
2022-04-19 11:51:54 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+from typing import Text
+
+
+Text = 'Вітаємо в калькуляторі майбутнього'
+print(Text)
+Number1 =int(input('Введіть перше число: '))
+Number2 = int(input('Введіть друге число:'))
+Sum = Number1 + Number2
+print('Сума ваших чисел дорівнює' ,Sum)
