e26198477ddceb8ecd4c3a12e2233746eacd271e
Maksimaka7 <103311378+Maksimaka7@users.noreply.github.com>
2022-04-19 18:59:32 +0300

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,15 @@
-from typing import Text
-
-
-Text = 'Вітаємо в калькуляторі майбутнього'
-print(Text)
-Number1 =int(input('Введіть перше число: '))
-Number2 = int(input('Введіть друге число:'))
-Sum = Number1 + Number2
-print('Сума ваших чисел дорівнює' ,Sum)
+Text_1 = 'Вітаємо в калькуляторі майбутнього'
+print(Text_1)
+Number_1 =int(input('Введіть перше число: '))
+Number_2 = int(input('Введіть друге число: '))
+oper = input('Оберіть одну із операцій +,-,*,/')
+if oper == '+':
+    print(Number_1+Number_2)
+elif oper == '-':
+    print(Number_1-Number_2)
+elif oper == '*':
+    print(Number_1*Number_2)
+elif oper == '/':
+    print(Number_1/Number_2)
+else:
+    print('error')
